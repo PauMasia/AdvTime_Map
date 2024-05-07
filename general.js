0b1b51ae92e2c9af42211850481b06e1b33a5833
PauMasià <dears2004pau@gmail.com>
2024-05-07 10:41:56 +0200

    Adicion JS, no testeado

--- general.js
+++ general.js
@@ -1,36 +1,30 @@
-//const slots = document.querySelectorAll('#div_zones #slots');
-//experimental
-// document.addEventListener('DOMContentLoaded', function(){
-//     var zones= document.querySelectorAll('.slots');
-// var overlay=document.querySelectorAll('.slots');
-// zones.forEach(function(text,index) {
-//     text.addEventListener('click', function() {
-//         var menu = overlay[index];
-//         toggleMenu(menu);
-//     });
-// });
-// function toggleMenu(menu) {
-//     overlay.forEach(function(m) {
-//         if (m === menu) {
-//             m.style.display = (m.style.display === 'none' || m.style.display === '') ? 'block' : 'none';
-//         } else {
-//             m.style.display = 'none';
-//         }
+
+// window.onload= document.getElementById('backOfMessage').style.display= 'inline';
+// onclick.document.getElementById('cloud_forest')=document.getElementById('cloud_forest_img');
+// onclick.document.getElementsByClassName('slots')=document.getElementById('overlay').style.display='inline';
+// // document.getElementsByClassName('slots')
+
+// var id_zone = onclick.document.addEventListener
+
+// //
+// var slotsElements = document.getElementsByClassName('slots');
+// for (var i = 0; i < slotsElements.length; i++) {
+//     slotsElements[i].addEventListener('click', function() {
+//         // Setting the display style property of an element with id 'overlay' to 'inline'
+//         document.getElementById('overlay').style.display = 'inline';
 //     });
 // }
-// });
-window.load= document.getElementById('backOfMessage').style.display= 'inline';
-onclick.document.getElementById('cloud_forest')=document.getElementById('cloud_forest_img');
-onclick.document.getElementsByClassName('slots')=document.getElementById('overlay').style.display='inline';
-document.getElementsByClassName('slots')
 
-
-
-//
-var slotsElements = document.getElementsByClassName('slots');
-for (var i = 0; i < slotsElements.length; i++) {
-    slotsElements[i].addEventListener('click', function() {
-        // Setting the display style property of an element with id 'overlay' to 'inline'
-        document.getElementById('overlay').style.display = 'inline';
-    });
+//falta testear, pon un id para una variable que vaya a existir para testear, name_menu+ '_info' para crear los menus de info de cada zona
+var name_menu= null;
+function select_click(singleSlot){
+    if(singleSlot.target.classList.contains('slots')){
+        name_menu= singleSlot.target.id;
+    }
+    document.getElementById (name_menu)
 }
+
+var id_zone= document.querySelectorAll('.slots');
+id_zone.forEach(function(select_zone){
+    select_zone.addEventListener('click',select_click);
+});
